026acfd31866d998cdc1e714a508c09bad13ae6f
me <me@DESKTOP-5JKLOSI>
2021-04-10 13:18:53 +0900

    Commit

---
+++ sec/src/main/java/com/example/board/Controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.board.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+	@RequestMapping("login")
+	public void login() {
+		System.out.println("Login Page");
+	}
+	
+	
+}
